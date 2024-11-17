f47329a97ee05040425db123547d87b423abac16
Maarten <maartendonkersloot@gmail.com>
2024-11-17 18:55:50 +0100

    Added reference structure

--- Tests/PresentationTests/obj/Debug/net9.0/PresentationTests.AssemblyInfo.cs
+++ Tests/PresentationTests/obj/Debug/net9.0/PresentationTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PresentationTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d2793a9b4ca699485bc1cd31e4dc3b8ce36eed2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PresentationTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PresentationTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
